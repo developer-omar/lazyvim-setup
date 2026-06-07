1763128c32b0e4e88ce411fda280e09ad96a633f
Omar Enrique <omar.e.geek@gmail.com>
2026-06-06 23:49:18 -0400

    installing Laravel plugin

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,37 +3,60 @@
 -- blink.cmp is a completion plugin with support for LSPs, cmdline, signature help, and snippets.
 
 return {
-  "saghen/blink.cmp",
-  dependencies = {
-    "rafamadriz/friendly-snippets",
+  {
+    "saghen/blink.compat",
+    -- use v2.* for blink.cmp v1.*
+    version = "2.*",
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
   },
-  opts = {
-    keymap = {
-      -- <c-e>: Hide menu
-      -- <c-k>: Toggle signature help
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      preset = "enter",
-      ["<c-k>"] = { "select_prev", "fallback" },
-      ["<c-j>"] = { "select_next", "fallback" },
-
-      -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-      --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
     },
-    sources = {
-      providers = {
-        snippets = {
-          opts = {
-            friendly_snippets = true,
-            -- Adding framework snippets to a filetype
-            extended_filetypes = {
-              typescript = { "angular" },
-              html = { "angular" },
+    opts = {
+      keymap = {
+        -- <c-e>: Hide menu
+        -- <c-k>: Toggle signature help
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        preset = "enter",
+        ["<c-k>"] = { "select_prev", "fallback" },
+        ["<c-j>"] = { "select_next", "fallback" },
+        -- Works when luasnip is selected as preset in snippets
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      },
+      sources = {
+        providers = {
+          snippets = {
+            opts = {
+              friendly_snippets = true,
+              -- Adding framework snippets to a filetype
+              extended_filetypes = {
+                typescript = { "angular" },
+                html = { "angular" },
+              },
             },
           },
+          laravel = {
+            name = "laravel",
+            module = "blink.compat.source",
+            score_offset = 95, -- show at a higher priority than lsp
+          },
         },
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
       },
-      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+      snippets = {
+        -- Use neovim engine for snippets, posible values default, luasnip
+        preset = "luasnip",
+      },
     },
   },
 }
